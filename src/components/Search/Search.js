bcbc1c19964f69eb3fb037cc15ca6084a0192a83
Ashish Malla <ashishmalla45@gmail.com>
2021-06-17 17:46:03 -0600

    Add header tag with title in it

---
+++ src/components/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='Search'>
+      <header>
+        <h1>Repo Creep🔍</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Search
