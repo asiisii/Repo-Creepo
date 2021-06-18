03fbe029e784464d020b2a474cab163908034803
Ashish Malla <ashishmalla45@gmail.com>
2021-06-18 00:26:21 -0600

    Use map iterator method to clean api data

--- src/apiData/cleanUpApiData.js
+++ src/apiData/cleanUpApiData.js
@@ -1,6 +1,20 @@
 
 const cleanUpApiData = repos => {
   console.log(repos);
+  const details = repos.map(repo => {
+    return {
+      id: repo.id,
+      username: repo.owner.login,
+      imgUrl: repo.owner.avatar_url,
+      github: repo.owner.url,
+      star: repo.stargazers_count,
+      language: repo.language,
+      repositoryName: repo.full_name,
+      description: repo.description,
+      viewRepo: repo.html_url,
+    }
+  })
+  console.log(details)
 }
 
 export default cleanUpApiData
