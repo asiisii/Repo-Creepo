e8b9c51a37a06e95168d3bfcb25f3a8fe032acd5
Ashish Malla <ashishmalla45@gmail.com>
2021-06-17 18:55:00 -0600

    Update lang's estring elements to obj element type

--- src/components/Form/languagesList.js
+++ src/components/Form/languagesList.js
@@ -2,31 +2,107 @@
 // source: https://www.benfrederickson.com/ranking-programming-languages-by-github-users/
 
 const langs = [
-  'JavaScript', 
-  'Python', 
-  'Java', 
-  'C++', 
-  'C', 
-  'PHP', 
-  'C#', 
-  'Shell', 
-  'Go', 
-  'TypeScript', 
-  'Ruby', 
-  'Jupiter Notebook', 
-  'Objective-C', 
-  'Swift', 
-  'Kotlin', 
-  'R', 
-  'Scala', 
-  'Rust' , 
-  'Lua', 
-  'Matlab', 
-  'PowerShell',
-  'CoffeeScript', 
-  'Perl', 
-  'Groovy', 
-  'Haskell' 
+  {
+    label: 'JavaScript', 
+    value: 'javascript'
+  },
+  {
+    label: 'Python', 
+    value: 'python'
+  },
+  {
+    label: 'Java', 
+    value: 'java'
+  },
+  {
+    label: 'C++', 
+    value: 'c++'
+  },
+  {
+    label: 'C', 
+    value: 'c'
+  },
+  {
+    label: 'PHP', 
+    value: 'php'
+  },
+  {
+    label: 'C#', 
+    value: 'c#'
+  },
+  {
+    label: 'Shell', 
+    value: 'shell'
+  },
+  {
+    label: 'Go', 
+    value: 'go'
+  },
+  {
+    label: 'TypeScript', 
+    value: 'typescript'
+  },
+  {
+    label: 'Ruby', 
+    value: 'ruby'
+  },
+  {
+    label: 'Jupiter Notebook', 
+    value: 'jupiter'
+  },
+  {
+    label: 'Objective-C', 
+    value: 'bbjective-c'
+  },
+  {
+    label: 'Swift', 
+    value: 'swift'
+  },
+  {
+    label: 'Kotlin', 
+    value: 'kotlin'
+  },
+  {
+    label: 'R', 
+    value: 'r'
+  },
+  {
+    label: 'Scala', 
+    value: 'scala'
+  },
+  {
+    label: 'Rust', 
+    value: 'rust'
+  },
+  {
+    label: 'Lua', 
+    value: 'lua'
+  },
+  {
+    label: 'Matlab', 
+    value: 'matlab'
+  },
+  {
+    label: 'PowerShell', 
+    value: 'powershell'
+  },
+  {
+    label: 'CoffeeScript', 
+    value: 'coffeeScript'
+  },
+  {
+    label: 'Perl', 
+    value: 'perl'
+  },
+  {
+    label: 'Groovy', 
+    value: 'groovy'
+  },
+  {
+    label: 'Haskell', 
+    value: 'haskell'
+  }
+
 ]
 
 export default langs
