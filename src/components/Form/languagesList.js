1f9774d88a63d4a0e2a8ed161cddffac387b1484
Ashish Malla <ashishmalla45@gmail.com>
2021-06-17 18:30:22 -0600

    Add github languages data

---
+++ src/components/Form/languagesList.js
@@ -0,0 +1,32 @@
+// list of programming langauges in GitHub
+// source: https://www.benfrederickson.com/ranking-programming-languages-by-github-users/
+
+const langs = [
+  'JavaScript', 
+  'Python', 
+  'Java', 
+  'C++', 
+  'C', 
+  'PHP', 
+  'C#', 
+  'Shell', 
+  'Go', 
+  'TypeScript', 
+  'Ruby', 
+  'Jupiter Notebook', 
+  'Objective-C', 
+  'Swift', 
+  'Kotlin', 
+  'R', 
+  'Scala', 
+  'Rust' , 
+  'Lua', 
+  'Matlab', 
+  'PowerShell',
+  'CoffeeScript', 
+  'Perl', 
+  'Groovy', 
+  'Haskell' 
+]
+
+export default langs
