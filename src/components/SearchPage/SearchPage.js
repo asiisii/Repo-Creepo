8194d71ae0eea45e61788b95444b044d1b2a53eb
Ashish Malla <ashishmalla45@gmail.com>
2021-06-19 14:05:07 -0600

    Update the Search component name to SearchPage

---
+++ src/components/SearchPage/SearchPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from '../Form/Form'
+
+const SearchPage = () => {
+  return (
+    <div className='Search'>
+      <header>
+        <h1>Repo Creep🔍</h1>
+      </header>
+      <Form />
+    </div>
+  )
+}
+
+export default SearchPage
