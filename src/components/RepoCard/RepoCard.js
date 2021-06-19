ef8911e41f1b3fa62021719cfb9e01c7e2111734
Ashish Malla <ashishmalla45@gmail.com>
2021-06-19 13:56:10 -0600

    Update repoData with store repoData instead of the props

--- src/components/RepoCard/RepoCard.js
+++ src/components/RepoCard/RepoCard.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import './RepoCard.css'
-const RepoCard = ({repoApiData}) => {
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+const RepoCard = () => {
+  const repoData = useSelector(store => store.repoData)
+
   console.log(`in repocard`)
-  // console.log(lang, repoName, repoApiData)
+  // console.log(lang, repoName, repoData)
   // const generateCards = () => {
-    const cardData = repoApiData.map(repoData => {
+    const cardData = repoData.map(repoData => {
       return (
-        <div 
+        <article 
         className='card' 
-        onClick={(e) => getRepoID(e)}
+        // onClick={(e) => getRepoID(e)}
         >
-          <button id={repoData.id}>👁</button>
+          <Link 
+          // id={repoData.id}
+          to={`/detailsPage/${repoData.id}`}
+          // onClick={(e) => getRepoID(e)}
+          // component={DetailsPage }
+          
+           >
+             👁
+             </Link>
           <h2>{repoData.repositoryName}</h2>
           <h2>{repoData.star}</h2>
           <h2>{repoData.language}</h2>
           <h2>{repoData.username}</h2>
           {/* <h2>{repoData.viewRepo}</h2> */}
-        </div>
+        </article>
     )
     })
     
