e50549dab4e39754e30222fa52a5ec5a6745ee9c
Ashish Malla <ashishmalla45@gmail.com>
2021-06-19 10:12:25 -0600

    Remove generateCards function to make it just a variable

--- src/components/RepoCard/RepoCard.js
+++ src/components/RepoCard/RepoCard.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
-const RepoCard = ({lang, repoName, repoApiData}) => {
+const RepoCard = ({repoApiData}) => {
   console.log(`in repocard`)
-  console.log(lang, repoName, repoApiData)
-  const generateCards = () => {
-    return repoApiData.map(repoData => {
+  // console.log(lang, repoName, repoApiData)
+  // const generateCards = () => {
+    const cardData = repoApiData.map(repoData => {
       return (
-
-        <>
-        {/* <h1>{repoName}</h1> */}
-        <h2>{repoData.repositoryName}</h2>
-        <h2>{repoData.star}</h2>
-        <h2>{repoData.language}</h2>
-        <h2>{repoData.username}</h2>
-        {/* <h2>{repoData.viewRepo}</h2> */}
-        <p>{repoData.id}</p>
-   </>
+        <div className='card' id={repoData.id}>
+          <h2>{repoData.repositoryName}</h2>
+          <h2>{repoData.star}</h2>
+          <h2>{repoData.language}</h2>
+          <h2>{repoData.username}</h2>
+          {/* <h2>{repoData.viewRepo}</h2> */}
+        </div>
     )
     })
-  }
+    // return cardData
+  // }
   return (
-    generateCards()
+    <section className='cards-grids'>
+      {cardData}
+    </section>
   )
 }
 
