313a3672177bb06b7b74ca0b46bae92d8d6c8576
Ashish Malla <ashishmalla45@gmail.com>
2021-06-19 13:58:19 -0600

    Add a route to details page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,30 @@ import React from 'react'
 import './App.css'
 import { Route, Switch } from 'react-router-dom'
 import Search from '../Search/Search'
+import { useSelector } from 'react-redux';
+import DetailsPage from '../DetailsPage/DetailsPage'
 
 const App = () => {
+  // const repoData = useSelector(store => store.repoData)
+  // console.log(repoData)
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={Search} />
-      </Switch>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Search} />
+          <Route 
+          path='/detailsPage/:id' 
+          render={({match}) => {
+            return (
+              <DetailsPage 
+                id={match.params.id}
+                // repoData={repoData}
+              />  
+
+            )
+          }}
+          />
+        </Switch>
+      </div>
   )
 }
 
