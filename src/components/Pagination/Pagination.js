810516c8f64808dbdfc0c9005210aabb2751554f
Ashish Malla <ashishmalla45@gmail.com>
2021-06-20 16:08:56 -0600

    Add next and previous button functionality to update the global RepoData

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,31 +1,70 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { fetchRepoData } from '../../apiData/apiCalls'
+import storeRepoDetails from '../../redux/action'
+import cleanUpApiData from '../../apiData/cleanUpApiData'
 import './Pagination.css'
 
-const Pagination = () => {
+const Pagination = ({repoName, lang, sort, setStatusCode, setFetchedError}) => {
+
+  let [pageNum, setPageNum] = useState(1)
+
   const dispatch = useDispatch()
   const repoData = useSelector(store => store.repoData)
-  const [pageNum, setPageNum] = useState(1)
 
     // if repoData.length is 0 then disabled both buttons
   // if pageNum is 1 then Previous button is disabled 
   // if repoData.length < 30 then make the Next button disabled
   const handleNextBtnClick = () => {
-    setPageNum(pageNum++)
+    setPageNum(pageNum + 1)
+   setFetchedError(false)
+    fetchRepoData(repoName, lang, sort, pageNum)
+      .then(response => {
+       setStatusCode(response.status)
+        return response.json()
+      })
+      .then(data => {
+        const repoDetails = cleanUpApiData(data.items)
+        dispatch(storeRepoDetails(repoDetails))
+      })
+      .catch(() =>setFetchedError(true))
   }
 
   const handlePreviousBtnClick = () => {
-    setPageNum(pageNum--)
+    setPageNum(pageNum - 1)
+   setFetchedError(false)
+    fetchRepoData(repoName, lang, sort, pageNum)
+      .then(response => {
+       setStatusCode(response.status)
+        return response.json()
+      })
+      .then(data => {
+        const repoDetails = cleanUpApiData(data.items)
+        dispatch(storeRepoDetails(repoDetails))
+      })
+      .catch(() =>setFetchedError(true))
   }
   
-
+console.log(pageNum);
   return (
+    <>
     <section className='Pagination'>
-      {pageNum > 1 ? <button className='previous'> 👈 Previous Page</button> : null}
-      <p>Page Num {pageNum}</p>
-      {repoData.length === 30 ? <button className='next'>Next Page 👉</button> : null }
+      {pageNum > 1 ? 
+      <button className='previous'
+      onClick={handlePreviousBtnClick}
+      > 👈 Previous Page</button> : 
+      null}
+
+      <p><span>Page Num: </span> {pageNum}</p>
+
+      {repoData.length === 30 ? 
+      <button className='next'
+      onClick={handleNextBtnClick}
+      >Next Page 👉</button> : 
+      null }
     </section>
     
+      </>
   )
 }
 
