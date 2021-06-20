b0de3c110b8ff5a8b3ba3577c08a36e1e67a7d46
Ashish Malla <ashishmalla45@gmail.com>
2021-06-20 15:18:49 -0600

    Add conditional rendering to both pagination buttons

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import './Pagination.css'
 
 const Pagination = () => {
   const dispatch = useDispatch()
   const repoData = useSelector(store => store.repoData)
   const [pageNum, setPageNum] = useState(1)
 
+    // if repoData.length is 0 then disabled both buttons
   // if pageNum is 1 then Previous button is disabled 
   // if repoData.length < 30 then make the Next button disabled
-  const updateRepoData = () => {
+  const handleNextBtnClick = () => {
+    setPageNum(pageNum++)
+  }
 
+  const handlePreviousBtnClick = () => {
+    setPageNum(pageNum--)
   }
   
 
   return (
-    <React.Fragment>
-      <button className='previous'>Previous Page</button>
-      <button className='next'>Next Page</button>
-    </React.Fragment>
+    <section className='Pagination'>
+      {pageNum > 1 ? <button className='previous'> 👈 Previous Page</button> : null}
+      <p>Page Num {pageNum}</p>
+      {repoData.length === 30 ? <button className='next'>Next Page 👉</button> : null }
+    </section>
     
   )
 }
