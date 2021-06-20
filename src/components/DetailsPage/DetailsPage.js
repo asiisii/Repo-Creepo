50796c2082692693c3f253a9804df2c45013eb1b
Ashish Malla <ashishmalla45@gmail.com>
2021-06-19 18:40:34 -0600

    Add a conditional rendering for details page

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-
+import { Redirect } from 'react-router'
 const  DetailsPage = ({id}) => {
   const repoData = useSelector(store => store.repoData)
   // console.log(repoData)
@@ -9,16 +9,21 @@ const  DetailsPage = ({id}) => {
   const getCurrentRepo = repoData.find(repo => parseInt(id) === repo.id)
   console.log(getCurrentRepo);
   return (
+      <>
+      {repoData.length ? 
     <section className='DetailsPage'>
       <img src={getCurrentRepo.imgUrl} />
       <h1>Username: {getCurrentRepo.username}</h1>
       <h1>User github: {getCurrentRepo.github}</h1>
+      <p>Full repository name: {getCurrentRepo.repositoryName}</p>
+      <h1>Stars: {getCurrentRepo.star}</h1>
       <h1>Language: {getCurrentRepo.language}</h1>
       <h1>View Repo: {getCurrentRepo.viewRepo}</h1>
-      <h1>Stars: {getCurrentRepo.star}</h1>
-      <p>Description: {getCurrentRepo.description}</p>
-      <p>Full repository name: {getCurrentRepo.repositoryName}</p>
+      <p>Description: {getCurrentRepo.description}</p> 
     </section>
+      : <Redirect to='/' />
+    }
+    </>
   )
 }
 
