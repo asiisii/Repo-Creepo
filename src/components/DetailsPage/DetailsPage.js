26a696f5ff77ea1903a32fcfc7c9d3fd30a1e0d1
Ashish Malla <ashishmalla45@gmail.com>
2021-06-20 02:48:19 -0600

    Add img link to gsearch page

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router'
+import { Link } from 'react-router-dom';
+import backbtn from '../../assests/back-button.png'
+import './DetailsPage.css'
+
 const  DetailsPage = ({id}) => {
   const repoData = useSelector(store => store.repoData)
   // console.log(repoData)
@@ -9,21 +13,31 @@ const  DetailsPage = ({id}) => {
   const getCurrentRepo = repoData.find(repo => parseInt(id) === repo.id)
   console.log(getCurrentRepo);
   return (
-      <>
+      <React.Fragment>
+        <Link to='/'>
+        <img src={backbtn} alt='back-button'  className='back-btn'/>
+      </Link>
       {repoData.length ? 
     <section className='DetailsPage'>
-      <img src={getCurrentRepo.imgUrl} />
-      <h1>Username: {getCurrentRepo.username}</h1>
-      <h1>User github: {getCurrentRepo.github}</h1>
-      <p>Full repository name: {getCurrentRepo.repositoryName}</p>
-      <h1>Stars: {getCurrentRepo.star}</h1>
-      <h1>Language: {getCurrentRepo.language}</h1>
-      <h1>View Repo: {getCurrentRepo.viewRepo}</h1>
-      <p>Description: {getCurrentRepo.description}</p> 
+      <div className='details-section'>
+        <article className='user-info'>
+          <img src={getCurrentRepo.imgUrl} />
+          <h1>@{getCurrentRepo.username}</h1>
+        </article>
+        <article className='repo-info'>
+          <h1>User github: {getCurrentRepo.github}</h1>
+          <h1>Full repository name: {getCurrentRepo.repositoryName}</h1>
+          <h1>Stars: {getCurrentRepo.star}</h1>
+          <h1>Language: {getCurrentRepo.language}</h1>
+          <h1>View Repo: {getCurrentRepo.viewRepo}</h1>
+          <h1>Description: {getCurrentRepo.description}</h1> 
+          <h1>View Repo: {getCurrentRepo.viewRepo}</h1>
+        </article>
+      </div>
     </section>
       : <Redirect to='/' />
     }
-    </>
+    </React.Fragment>
   )
 }
 
