e987514eb6ae19adb5317753ff71c392db22f8d0
Ashish Malla <ashishmalla45@gmail.com>
2021-06-20 23:27:09 -0600

    Add dummy data for repo-creepo repository

---
+++ cypress/fixtures/repo-crepo.js
@@ -0,0 +1,17 @@
+export default {
+  "items": [
+    {
+      "id": 377966192,
+      "owner": {
+        "login": "ashish",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 0,
+      "language": "JavaScript",
+      "name": "repo-Creepo",
+      "description": "App that lets user search Repository using Github API",
+      "html_url": "https://github.com/asiisii/Repo-Creepo"
+    }
+  ]
+}
