1dd7beb712852a633717f3ad3651095050617188
Ashish Malla <ashishmalla45@gmail.com>
2021-06-21 00:54:33 -0600

    Add test to verfiy its a controlled form

--- cypress/integration/searchpage_spec.js
+++ cypress/integration/searchpage_spec.js
@@ -2,19 +2,22 @@ describe('Search page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-  it('should display 30 repository cards', () => {
-    cy.get('button[value="Submit"]').click()
-      .get('input[name="repoName"]:invalid')
-      .invoke('prop', 'validationMessage')
-      .should('equal', 'Please fill out this field.')
-      .get('input[name="repoName"]').type('reactionary')
-      .get('button[value="Submit"]').click()
-      .get('.option-err')
-      .contains('Please fill out all the form')
+  it('should be a controlled form', () => {
+    cy.get('input[name="repoName"]').type('reactionary')
+      .should('have.value', 'reactionary')
+      .get('select').eq(0).select('c')
+      .should('have.value', 'c')
       .get('select').eq(0).select('javascript')
-      .get('button[value="Submit"]').click()
-      .get('.option-err')
-      .contains('Please fill out all the form')
+      .should('have.value', 'javascript')
+      .get('select').eq(1).select('No')
+      .should('have.value', '/')
+      .get('select').eq(1).select('Yes')
+      .should('have.value', '&sort=stars')
+  })
+
+  it('should display 30 repository cards', () => {
+    cy.get('input[name="repoName"]').type('reactionary')
+      .get('select').eq(0).select('javascript')
       .get('select').eq(1).select('Yes')
       // intercept fetch call and give reactionary repo dummy data
       .interceptReactionary()
