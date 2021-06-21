13bf97778b396ee2ff196db6e7c3d92d637a9f1d
Ashish Malla <ashishmalla45@gmail.com>
2021-06-21 00:19:49 -0600

    Update reactionaryPath with per_page=30 query

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,7 @@ import repoCreepo from '../fixtures/repo-crepo'
 import reactionary from '../fixtures/reactionary'
 
 const repoCreepoPath = 'https://api.github.com/search/repositories?q=repo-creepo+language:javascript&sort=stars&page=1'
-const reactionaryPath = 'https://api.github.com/search/repositories?q=reactionary+language:javascript&sort=stars&page=1'
+const reactionaryPath = 'https://api.github.com/search/repositories?q=reactionary+language:javascript&sort=stars&per_page=30&page=1'
 
 Cypress.Commands.add('interceptRepoCreepoCard', () => {
   cy.intercept(repoCreepoPath, repoCreepo)
