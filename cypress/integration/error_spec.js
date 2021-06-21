f7bb3e1d11e57131b1ecdc43723067813e55b105
Ashish Malla <ashishmalla45@gmail.com>
2021-06-20 22:37:11 -0600

    Add test for the search limit error message

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -8,6 +8,20 @@ describe('Error', () => {
         statusCode: 404
       })
       .get('button[value="Submit"]').click()
-      .get('.error-msg').contains('Sorry, we couldn\'t find repository you were looking for')
+      .get('.error-msg')
+      .contains('Sorry, we couldn\'t find repository you were looking for')
+  })
+
+  it.only('should display error message when reached the search limit', () => {
+    cy.visit('http://localhost:3000')
+      .get('input[name="repoName"]').type('repo-creepo')
+      .get('select').eq(0).select('javascript')
+      .get('select').eq(1).select('Yes')
+      .intercept('https://api.github.com/search/repositories?q=repo-creepo+language:javascript&sort=stars&page=1', {
+        statusCode: 403
+      })
+      .get('button[value="Submit"]').click()
+      .get('.error-msg')
+      .contains('Sorry, you\'ve reached the search limit. Please try again in an hour.')
   })
 })
