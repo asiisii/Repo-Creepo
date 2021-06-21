0cb0b98b5d4eeecff1225be48adbc5510bf79dc5
Ashish Malla <ashishmalla45@gmail.com>
2021-06-21 00:07:15 -0600

    Add reactionary dummy data for the interception

---
+++ cypress/fixtures/reactionary.js
@@ -0,0 +1,407 @@
+export default {
+  "items": [
+    {
+      "id": 1,
+      "owner": {
+        "login": "a",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 1,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 2,
+      "owner": {
+        "login": "b",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 2,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 3,
+      "owner": {
+        "login": "c",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 3,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 4,
+      "owner": {
+        "login": "d",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 4,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 5,
+      "owner": {
+        "login": "e",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 5,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 6,
+      "owner": {
+        "login": "f",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 6,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 7,
+      "owner": {
+        "login": "g",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 7,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 8,
+      "owner": {
+        "login": "h",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 8,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id":9,
+      "owner": {
+        "login": "i",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count":9,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 10,
+      "owner": {
+        "login": "j",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 10,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 11,
+      "owner": {
+        "login": "k",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 11,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 12,
+      "owner": {
+        "login": "l",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 12,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 13,
+      "owner": {
+        "login": "m",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 13,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 14,
+      "owner": {
+        "login": "n",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 14,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 15,
+      "owner": {
+        "login": "o",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 15,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 16,
+      "owner": {
+        "login": "p",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 16,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 17,
+      "owner": {
+        "login": "q",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 17,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 18,
+      "owner": {
+        "login": "r",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 18,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 19,
+      "owner": {
+        "login": "s",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 19,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 20,
+      "owner": {
+        "login": "t",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 20,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 21,
+      "owner": {
+        "login": "u",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 21,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 22,
+      "owner": {
+        "login": "v",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 22,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 23,
+      "owner": {
+        "login": "w",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 23,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 24,
+      "owner": {
+        "login": "x",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 24,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 25,
+      "owner": {
+        "login": "y",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 25,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 26,
+      "owner": {
+        "login": "z",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 26,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 27,
+      "owner": {
+        "login": "aa",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 27,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 28,
+      "owner": {
+        "login": "ab",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 28,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 29,
+      "owner": {
+        "login": "ac",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 29,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 30,
+      "owner": {
+        "login": "ad",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 30,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    },
+    {
+      "id": 31,
+      "owner": {
+        "login": "ae",
+        "avatar_url": "https://avatars.githubusercontent.com/u/36644181?v=4",
+        "url": "https://api.github.com/users/asiisii"
+      },
+      "stargazers_count": 31,
+      "language": "JavaScript",
+      "name": "Reactionary",
+      "description": "Dictionary App",
+      "html_url": "https://github.com/asiisii/Reactionary"
+    }
+  ]
+}
