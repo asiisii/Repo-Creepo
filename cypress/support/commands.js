c7ce901aa1e841f6691d6a7eca9450af051d8efa
Ashish Malla <ashishmalla45@gmail.com>
2021-06-21 01:45:39 -0600

    Add intercept method to intercept page 2 data

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,13 +1,22 @@
 import repoCreepo from '../fixtures/repo-crepo'
 import reactionary from '../fixtures/reactionary'
+import reactionary2 from '../fixtures/reactionary2'
 
 const repoCreepoPath = 'https://api.github.com/search/repositories?q=repo-creepo+language:javascript&sort=stars&page=1'
 const reactionaryPath = 'https://api.github.com/search/repositories?q=reactionary+language:javascript&sort=stars&page=1'
+const reactionary2Path = 'https://api.github.com/search/repositories?q=reactionary+language:javascript&sort=stars&page=2'
 
+//intercept for repoCreepo repository
 Cypress.Commands.add('interceptRepoCreepoCard', () => {
   cy.intercept(repoCreepoPath, repoCreepo)
 })
 
+//intercept for Reactionary repository page : 1
 Cypress.Commands.add('interceptReactionary', () => {
   cy.intercept(reactionaryPath, reactionary)
+})
+
+//intercept for Reactionary repository page : 2
+Cypress.Commands.add('interceptReactionary2', () => {
+  cy.intercept(reactionary2Path, reactionary2)
 })
